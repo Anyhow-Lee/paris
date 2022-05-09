f5d24590e677e45abeb730029227d0c65fed3775
Anyhow Lee <Anyhow_Lee@163.com>
2022-05-09 18:14:46 +0800

    s2 Excel

---
+++ src/pages/s2-table/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script>
+import { PivotSheet } from '@antv/s2'
+import s2DataConfig from './s2DataConfig'
+export default {
+  data() {
+    return {
+      s2DataConfig,
+    }
+  },
+  mounted() {
+    const container = document.getElementById('container')
+    const s2Options = {
+      width: 600,
+      height: 600,
+      totals: {
+        row: {
+          showGrandTotals: true,
+          showSubTotals: true,
+          //   reverseLayout: true,
+          //   reverseSubLayout: true,
+          subTotalsDimensions: ['province'],
+          calcTotals: {
+            aggregation: 'SUM',
+          },
+          calcSubTotals: {
+            aggregation: 'SUM',
+          },
+        },
+        col: {
+          showGrandTotals: true,
+          showSubTotals: true,
+          //   reverseLayout: true,
+          //   reverseSubLayout: true,
+          subTotalsDimensions: ['type'],
+          calcTotals: {
+            aggregation: 'SUM',
+          },
+          calcSubTotals: {
+            aggregation: 'SUM',
+          },
+        },
+      },
+    }
+    const s2 = new PivotSheet(container, s2DataConfig, s2Options)
+    s2.render()
+  },
+}
+</script>
+
+<style></style>
